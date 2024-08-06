d1db556d0bfd5b4e35747e8efed7c2f91e06bfa6
RamaiyaA <ramaiya.a@cisinlabs.com>
2024-08-06 17:21:07 +0530

    chapter -1 origin change

--- src/app.js
+++ src/app.js
@@ -10,8 +10,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST'],
+    origin: 'https://rn-youtube-frontend.vercel.app/',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   },
 });
